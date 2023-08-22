4895ecfa4232e34fa13ef079ca829a662b89fbfc
Adamtech101 <adamahmedtech@gmail.com>
2023-08-22 15:56:58 +0100

    various

---
+++ java.js
@@ -0,0 +1,42 @@
+homework lesson7
+
+let name = "Saul Goodman";
+let state = "New Mexico";
+
+let result = name + "is from" + "state";
+
+console.log(result);
+
+
+
+
+let age = 40;
+
+console.log(name + "is" + age + "years old.")
+
+
+
+
+let sport = "football"
+let ranking = 1;
+let where = "world"
+
+console.log(sport + "is the number" + ranking + "most popular game in the" + where)
+
+
+
+homework lesson8
+
+let myarray = ["eggs", "milk", "bread", "cheese", "yoghurt", "juice", "pastry", "crisps", "pasta", "notebook", "fairy liquid", "beans", "mayo", "dates", "gum"]
+
+console.log(myarray.pop);
+
+console.log(myarray);
+
+
+let myarray = ["eggs", "milk", "bread", "cheese", "yoghurt", "juice", "pastry", "crisps", "pasta", "notebook", "fairy liquid", "beans", "mayo", "dates", "gum"]
+
+const count = myarray.push('cashews');
+console.log(count);
+
+console.log(myarray)
